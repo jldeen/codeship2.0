6b281eb9e4ba7754c0810423458e84fd357836c1
Jessica Deen <jedeen@outlook.com>
2017-03-15 12:34:25 -0700

    Modified acs deploy

--- deployment/acs_deploy.sh
+++ deployment/acs_deploy.sh
@@ -9,38 +9,30 @@ az login \
     -p $password \
     --tenant $tenant
 
-# Azure variables
-rgname="JDTest"
-servicename="ACSJDDemo"
-orchestrator="Swarm" #other options include kubernetes and dcos
-dnsprefix="jdacs2"
-loc="centralus"
-
 # Group creation
 az group create \
-    -l $loc \
-    -n $rgname
+    -l $Location \
+    -n $Resource
 echo "Created Resource Group:" $rgname
 
 # ACS Creation for Docker Swarm
 az acs create \
-    -g $rgname \
-    -n $servicename \
-    -d $dnsprefix \
-    --orchestrator-type $orchestrator \
+    -g $Resource \
+    -n $Dervicename \
+    -d $Dnsprefix \
+    --orchestrator-type $Orchestrator \
     --generate-ssh-keys \
     --verbose
 
-
 # Grab the fully qualified domain name in an environment variable
-fqdn=$dnsprefix.$loc.cloudapp.azure.com
+fqdn=$Dnsprefix.$Location.cloudapp.azure.com
 
 # Copy FQDN to host from container and to .gitignore
 echo $fqdn > /deploy/fqdn
 echo fqdn >> /deploy/.gitignore
 
 # Copy Private Key to host from container and to .gitignore
-cp /root/.ssh/id_rsa
+cp /root/.ssh/id_rsa /deploy/id_rsa
 echo id_rsa >> /deploy/.gitignore
 
 # Confirm FQDN is captured and print to screen
