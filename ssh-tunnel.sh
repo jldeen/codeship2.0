3cf42e6b9a199fb0e96461f91a72680a4de59781
Jessica Deen <jedeen@outlook.com>
2017-03-17 22:17:56 -0700

    Added sh for use with dockerfile

---
+++ ssh-tunnel.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Create SSH Tunnel - Add on boot 
+
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- docker "$@"
+fi
+
+# if our command is a valid Docker subcommand, let's invoke it through Docker instead
+# (this allows for "docker run docker ps", etc)
+if docker help "$1" > /dev/null 2>&1; then
+	set -- docker "$@"
+fi
+
+ssh -fNL 2375:localhost:2375 -p 2200 azureuser@$(cat fqdn) -o StrictHostKeyChecking=no -o ServerAliveInterval=240 &>/dev/null
+
+exec "$@"
