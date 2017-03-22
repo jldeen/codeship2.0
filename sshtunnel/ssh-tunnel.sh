d12d449c22e0a5411e6e60c9777e9aa796904825
Jessica Deen <jedeen@outlook.com>
2017-03-22 16:54:36 -0700

    Udpated with variables for scalabiltiy

--- sshtunnel/ssh-tunnel.sh
+++ sshtunnel/ssh-tunnel.sh
@@ -4,17 +4,17 @@
 		n=0
 		until [ $n -ge 5 ]
 		do
-			ssh -fNL 2375:localhost:2375 -p 2200 azureuser@$(cat fqdn) -o StrictHostKeyChecking=no -o ServerAliveInterval=240 && echo "ACS SSH Tunnel successfully opened..." && break
+			ssh -fNL $local_port:localhost:$remote_port -p 2200 azureuser@$(cat fqdn) -o StrictHostKeyChecking=no -o ServerAliveInterval=240 &>/dev/null && echo "ACS SSH Tunnel successfully opened..." && break
 			n=$((n+1)) &>/dev/null && echo "SSH tunnel is not ready. Retrying in 5 seconds..."
 			sleep 5
 		done 
 
-# Check for Swarm Cluster Node availability, if errors, try again up to 5 times - only necessary if ACS Cluster was recently deployed
+# Check for ACS Cluster Node availability, if errors, try again up to 5 times - only necessary if ACS Cluster was recently deployed
 	n=0
 	until [ $n -ge 5 ]
 	do
-		docker info | grep 'Nodes: [1-9]' &>/dev/null && echo "Swarm cluster is ready..." && break
-		n=$((n+1)) &>/dev/null && echo "Swarm cluster is not ready. Retrying in 45 seconds..."
+		docker info | grep 'Nodes: [1-9]' &>/dev/null && echo "$Orchestrator cluster is ready..." && break ## if docker Swarm
+		n=$((n+1)) &>/dev/null && echo "$Orchestrator cluster is not ready. Retrying in 45 seconds..."
 		sleep 45
 	done 
 
@@ -25,13 +25,24 @@
 	fi
 
 # If our command is a valid Docker subcommand, invoke it through Docker instead - (this allows for "docker run docker ps", etc)
-	if docker help "$1" > /dev/null 2>&1; then
+	if docker help "$1" &>/dev/null; then
 		set -- docker "$@"
 	fi
 # Out to end user and execute docker command
 	echo "Reminder: Your web applications can be viewed here: $(cat agents)"
-	exec "$@" 
+	echo "Executing supplied $Orchestrator command: '$@'"
+	eval "$@" && echo "'$@' completed" 
 
 	# && echo "Supplied command successfully completed..." [ STOPPED HERE; FIGURE OUT HOW TO ECHO SUCCESS TO END USER]
 
-# ssh output to nothing &>/dev/null
+	## if [ "$Orchestrator" -eq "Swarm" ] then; 
+		#docker info | grep 'Nodes: [1-9]' &>/dev/null && echo "$Orchestrator cluster is ready..." && break
+	 # fi
+
+	## if [ "$Orchestrator" -eq "Kubernetes" ] then; 
+		#docker info | grep 'Nodes: [1-9]' &>/dev/null && echo "$Orchestrator cluster is ready..." && break
+	 # fi
+
+	## if [ "$Orchestrator" -eq "dcos" ] then; 
+		#docker info | grep 'Nodes: [1-9]' &>/dev/null && echo "$Orchestrator cluster is ready..." && break
+	 # fi
