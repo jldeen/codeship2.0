f10f38431c7863d90cc9a10ecd0e20b6ff99e035
Jessica Deen <jedeen@outlook.com>
2017-03-23 15:43:47 -0700

    Updated azure.env to remove spaces from echo

--- local_scripts/create_serviceprincipal.sh
+++ local_scripts/create_serviceprincipal.sh
@@ -4,60 +4,61 @@
 # To view the available roles, see https://docs.microsoft.com/azure/active-directory/role-based-access-built-in-roles Default recommended is Contributor, which can manage everything except access
 
 # Read User Input to capture variables
-    echo "This script will create a Service Principal (SPN) for Azure."
-    echo
-    echo "Enter a name for your SPN and press [ENTER]: "
-    read name
-    echo "The name you entered is $name."
-    echo "Enter a password for your SPN and press [ENTER]: "
-    read -s password    
-    echo "Enter a role for your SPN and press [ENTER]. The default role suggested is Contributor: "
-    read role
-    echo "The role you entered is $role."
-    echo 
-    echo "Thank you for your input. Now proceeding with SPN creation..."
+echo "This script will create a Service Principal (SPN) for Azure."
+echo
+echo "Enter a name for your SPN and press [ENTER]: "
+read name
+echo "The name you entered is $name."
+echo "Enter a password for your SPN and press [ENTER]: "
+read -s password    
+echo "Enter a role for your SPN and press [ENTER]. The default role suggested is Contributor: "
+read role
+echo "The role you entered is $role."
+echo 
+echo "Thank you for your input. Now proceeding with SPN creation..."
 
 # Login - Complete this process using a browser
-    az login
+az login
 # Function for create_spn
-    create_spn () {
-        echo
-        echo "Creating your SPN now..."
-        # Capture tenant ID
-        tenant=$(az account show | jq -r '.tenantId')
+create_spn () {
+echo
+echo "Creating your SPN now..."
+# Capture tenant ID
+tenant=$(az account show | jq -r '.tenantId')
 
-    # Begin AD Service Principal Creation 
-        az ad sp create-for-rbac \
-            -n $name \
-            --password $password \
-            --role $role \
-            --verbose
+# Begin AD Service Principal Creation 
+az ad sp create-for-rbac \
+    -n $name \
+    --password $password \
+    --role $role \
+    --verbose
 
-    # Output service principal
-        echo "Successfully created Service Principal."
-        echo "==============Created Serivce Principal=============="
-        echo "spn=http://$name" 
-        echo "password=$password"
-        echo "tenant=$tenant"
+# Output service principal
+echo "Successfully created Service Principal."
+echo "==============Created Serivce Principal=============="
+echo "spn=http://$name" 
+echo "password=$password"
+echo "tenant=$tenant"
+echo 
 
-        spn=http://$name
+spn=http://$name
 
-    # Copy service principal to environment variables file
-        echo 'spn='$spn'
-        password='$password'
-        tenant='$tenant'
-        ' > azure.env
-        echo "azure.env created"
+# Copy service principal to environment variables file
+echo "spn=$spn
+password=$password
+tenant=$tenant
+" > azure.env
+echo "azure.env created"
 
-    # Add azure.env to .gitignore
-        echo "azure.env" >> .gitignore
-        echo "azure.env copied to .gitignore"
+# Add azure.env to .gitignore
+echo "azure.env" >> .gitignore
+echo "azure.env copied to .gitignore"
 
-    # Encrypt azure.env using CodeShip Jet
-        # jet encrypt [--key-path=codeship.aes] plain_file encrypted_file
-        jet encrypt azure.env azure.env.encrypted
-        echo "Successfully encrypted azure.env"
-        }
+# Encrypt azure.env using CodeShip Jet
+# jet encrypt [--key-path=codeship.aes] plain_file encrypted_file
+jet encrypt azure.env azure.env.encrypted
+echo "Successfully encrypted azure.env"
+}
 
 # Azure Subscription Selection
     # Check for multiple subscriptions
@@ -99,7 +100,7 @@
         shopt -s extglob #turn on extended pattern matching for +([0-9]) 
         while [ 1 ];
         do
-            read -p "Please make a selection:" menu_choice
+            read -p "Please make a selection and press [ENTER]:" menu_choice
 
     # Menu selection 
             case $menu_choice in
